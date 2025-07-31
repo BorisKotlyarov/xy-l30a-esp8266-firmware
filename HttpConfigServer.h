a7269b8ea3999ae36ca21f0cf49837a002120c92
Borys Kotliarov <9309706@gmail.com>
2025-07-31 18:15:43 +0300

    Srings replaced to char

--- HttpConfigServer.h
+++ HttpConfigServer.h
@@ -7,17 +7,20 @@
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 
-class HttpConfigServer {
+class HttpConfigServer
+{
 private:
     ESP8266WebServer server;
     String authUser;
     String authPass;
-    
-    SoftwareSerial* loraSerial = nullptr;
+
+    SoftwareSerial *loraSerial = nullptr;
     bool isSerialDebug = false;
     bool mqttConnected = false;
 
-    std::function<void(const String&, const String&, const String&, const String&, const String&, const String&, const String&)> saveCallback;
+    std::function<void(const char *, const char *, const char *, const char *,
+                       const char *, const char *, const char *)>
+        saveCallback;
     std::function<void()> resetCredentialsCallback;
 
     String _mqtt_ip = "";
@@ -26,7 +29,6 @@ private:
     String _mqtt_pass = "";
     String _client_id = "";
 
-
     void handleRoot();
     void handleSendCommand();
     void handleConfigPage();
@@ -34,28 +36,27 @@ private:
     void handleStatus();
     void handleNotFound();
     bool isAuthorized();
-    
 
 public:
     HttpConfigServer(int port = 80,
-        std::function<void(const String&, const String&, const String&, const String&, const String&, const String&, const String&)> credCb = nullptr,
-        std::function<void()> rstCredCb = nullptr);
-
+                     std::function<void(const char *, const char *, const char *, const char *,
+                                        const char *, const char *, const char *)>
+                         credCb = nullptr,
+                     std::function<void()> rstCredCb = nullptr);
 
     void begin();
     void loop();
 
     // MQTT - state setter
     void setMqttConnected(bool state);
-    
-    void setAuth(String user, String pwd);    
-    
-    void setLoraSerial(SoftwareSerial* serial);
 
-    void setIsSerialDebug(bool isDebug);    
+    void setAuth(String user, String pwd);
+
+    void setLoraSerial(SoftwareSerial *serial);
+
+    void setIsSerialDebug(bool isDebug);
 
     void setMQTT(String mqtt_ip, String mqtt_port, String mqtt_user, String mqtt_pass, String client_id);
 };
-
 
 #endif // HTTP_CONFIG_SERVER_H
